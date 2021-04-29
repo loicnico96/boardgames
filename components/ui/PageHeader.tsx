ab83737f261d359e13b216688403a8ee64840fa8
loicnico96 <loicnico96@gmail.com>
2021-04-29 19:16:26 +0300

    Add icon

--- components/ui/PageHeader.tsx
+++ components/ui/PageHeader.tsx
@@ -42,7 +42,23 @@ export default function PageHeader({ title }: PageHeaderProps) {
     <div className="PageHeader">
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <div className="Breadcrumbs">{title}</div>
       {user ? (
