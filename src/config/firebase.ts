f10ab08890846b06508839ef40294932e56430f4
loicnico96 <loicnico96@gmail.com>
2021-05-05 16:44:45 +0300

    Move source files to 'src' folder

---
+++ src/config/firebase.ts
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyCfF_VjlMza6tb1zL_PWtu5eCHHLkp0rPs",
+  authDomain: "roborally-e518d.firebaseapp.com",
+  databaseURL: "https://roborally-e518d.firebaseio.com",
+  projectId: "roborally-e518d",
+  storageBucket: "roborally-e518d.appspot.com",
+  messagingSenderId: "198561771188",
+  appId: "1:198561771188:web:c3f0b7379d0aa3554bb1e1",
+  measurementId: "G-BXSN5FMY1X",
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,6 @@ import ToastProvider from "components/providers/ToastProvider"
 import TranslationProvider from "components/providers/TranslationProvider"
 import { StoreProvider } from "lib/store/context"
 
-import "styles/globals.css"
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <TranslationProvider>
@@ -15,6 +13,27 @@ export default function App({ Component, pageProps }: AppProps) {
         <StoreProvider>
           <AuthProvider>
             <Component {...pageProps} />
+            <style jsx global>{`
+              html,
+              body,
+              #__next {
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                  Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+                  Helvetica Neue, sans-serif;
+                height: 100%;
+                margin: 0;
+                padding: 0;
+              }
+
+              a {
+                color: inherit;
+                text-decoration: none;
+              }
+
+              * {
+                box-sizing: border-box;
+              }
+            `}</style>
           </AuthProvider>
         </StoreProvider>
       </ToastProvider>
