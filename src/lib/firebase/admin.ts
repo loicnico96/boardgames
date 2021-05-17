bf352c27dd0ac46d352ede7dfa8a62a4ea272109
loicnico96 <loicnico96@gmail.com>
2021-05-17 10:21:50 +0300

    Setup Firebase Admin SDK

---
+++ src/lib/firebase/admin.ts
@@ -0,0 +1,12 @@
+import * as admin from "firebase-admin"
+
+import credentials from "config/credentials/firebase-admin"
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+    databaseURL: credentials.databaseUrl,
+  })
+}
+
+export const auth = admin.auth()
