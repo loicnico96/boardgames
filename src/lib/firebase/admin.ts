01088290be020d42ff5e36ff6ee335d5503fb836
loicnico96 <loicnico96@gmail.com>
2021-05-20 18:51:06 +0200

    Use environment variables for Firebase Admin

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,11 +1,15 @@
 import * as admin from "firebase-admin"
 
-import credentials from "config/credentials/firebase-admin"
+import firebaseConfig from "config/firebase"
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(credentials),
-    databaseURL: credentials.databaseUrl,
+    credential: admin.credential.cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      projectId: firebaseConfig.projectId,
+    }),
+    databaseURL: firebaseConfig.databaseURL,
   })
 }
 
