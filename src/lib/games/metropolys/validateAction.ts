a57478e8a36ace6508656e9d67be3ff0c8fc6fb2
loicnico96 <loicnico96@gmail.com>
2021-06-18 19:17:01 +0200

    [Metropolys] Validate action

--- src/lib/games/metropolys/validateAction.ts
+++ src/lib/games/metropolys/validateAction.ts
@@ -1,7 +1,49 @@
 import { int, object, oneOf } from "lib/utils/validation"
 
 import { MetropolysAction } from "./model/MetropolysAction"
-import { MetropolysState } from "./model/MetropolysState"
+import {
+  BUILDING_COUNT,
+  DISTRICT_COUNT,
+  getHighestBid,
+  getPlayer,
+  isDistrictAvailable,
+  MetropolysState,
+} from "./model/MetropolysState"
+
+export function checkPlayerCanPass(gameState: MetropolysState) {
+  const highestBid = getHighestBid(gameState)
+
+  if (!highestBid) {
+    throw Error("Cannot pass the first bid")
+  }
+}
+
+export function checkPlayerCanBid(
+  gameState: MetropolysState,
+  playerId: string,
+  district: number,
+  height: number
+) {
+  const highestBid = getHighestBid(gameState)
+  const player = getPlayer(gameState, playerId)
+
+  if (!player.buildings[height]) {
+    throw Error("Building is not available")
+  }
+
+  if (highestBid && highestBid.height >= height) {
+    throw Error("Building is not high enough")
+  }
+
+  if (!isDistrictAvailable(gameState, district)) {
+    throw Error("District is not available")
+  }
+
+  // TODO: Uncomment when district adjacency is added
+  // if (highestBid && !isAdjacent(district, highestBid.district)) {
+  //   throw Error("District is not adjacent")
+  // }
+}
 
 export function validateAction(
   gameState: MetropolysState,
@@ -10,14 +52,27 @@ export function validateAction(
 ): MetropolysAction {
   const typedAction = oneOf(
     object({
-      district: int(),
-      height: int(),
+      district: int({
+        min: 0,
+        max: DISTRICT_COUNT - 1,
+      }),
+      height: int({
+        min: 0,
+        max: BUILDING_COUNT - 1,
+      }),
       pass: false,
     }),
     object({
       pass: true,
     })
   )(action)
+
+  if (typedAction.pass) {
+    checkPlayerCanPass(gameState)
+  } else {
+    const { district, height } = typedAction
+    checkPlayerCanBid(gameState, playerId, district, height)
+  }
 
   return typedAction
 }
