79d21c8170ffa81bb069149a742d85b2f6d06b01
loicnico96 <loicnico96@gmail.com>
2021-07-28 22:27:57 +0200

    Implement player action API

--- src/pages/api/[game]/[room]/action/index.ts
+++ src/pages/api/[game]/[room]/action/index.ts
@@ -2,18 +2,53 @@ import { ApiError } from "lib/api/error"
 import { handle, readBody, readParam } from "lib/api/server"
 import { getUserId } from "lib/api/server/auth"
 import { HttpMethod, HttpStatus } from "lib/api/types"
+import { getClientRef, getServerRef } from "lib/db/collections"
+import { firestore } from "lib/firebase/admin"
+import { GameAction, GameState, getGameSettings } from "lib/games/GameSettings"
 import { GameType } from "lib/games/GameType"
 import { isEnum } from "lib/utils/enums"
 import { Param } from "lib/utils/navigation"
 import { any } from "lib/utils/validation"
 
-export async function playerAction(
+export async function playerAction<T extends GameType>(
   userId: string,
-  game: GameType,
+  game: T,
   roomId: string,
   action: unknown
 ): Promise<void> {
-  //
+  await firestore.runTransaction(async transaction => {
+    const clientRef = firestore.doc(getClientRef(game, roomId))
+    const serverRef = firestore.doc(getServerRef(game, roomId))
+    const serverDoc = await transaction.get(serverRef)
+    const gameState = serverDoc.data() as GameState<T> | undefined
+    if (!gameState) {
+      throw new ApiError(
+        HttpStatus.NOT_FOUND,
+        "This room does not exist or has been closed"
+      )
+    }
+
+    const { validateAction, resolvePlayerAction, resolveState } =
+      getGameSettings(game)
+
+    let validatedAction: GameAction<T>
+
+    try {
+      validatedAction = validateAction(gameState, userId, action)
+    } catch (error) {
+      throw new ApiError(HttpStatus.BAD_REQUEST, error.message)
+    }
+
+    let nextState = gameState
+
+    nextState = await resolvePlayerAction(nextState, userId, validatedAction)
+
+    transaction.update(clientRef, nextState)
+
+    nextState = await resolveState(nextState)
+
+    transaction.update(serverRef, nextState)
+  })
 }
 
 export default handle({
@@ -22,7 +57,7 @@ export default handle({
     const game = readParam(request, Param.GAME_TYPE)
 
     if (!isEnum(game, GameType)) {
-      throw new ApiError(HttpStatus.BAD_REQUEST, "Unknown game type")
+      throw new ApiError(HttpStatus.NOT_FOUND, "Unknown game type")
     }
 
     const roomId = readParam(request, Param.ROOM_ID)
