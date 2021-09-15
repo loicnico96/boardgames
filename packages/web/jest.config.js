a70f873387f65b5ba9d06bedbca42c388ba83a0e
loicnico96 <loicnico96@gmail.com>
2021-09-15 17:41:59 +0200

    Add top-level tsconfig.json

--- packages/web/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  collectCoverageFrom: ["**/*.{ts,tsx}"],
-  coverageReporters: ["html", "text-summary"],
-  moduleNameMapper: {
-    "^pages(.*)$": "<rootDir>/src/pages$1",
-  },
-  testEnvironment: "jsdom",
-}
