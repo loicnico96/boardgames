b50eaec9dcea4916145e4f53b65c98db2ec81c02
loicnico96 <loicnico96@gmail.com>
2021-11-03 19:31:14 +0100

    Add Cacao asset pre-loading

--- packages/web/src/components/games/cacao/index.tsx
+++ packages/web/src/components/games/cacao/index.tsx
@@ -1,16 +1,22 @@
 import { GameProvider } from "components/providers/GameProvider"
+import { ImageLoader } from "components/ui/ImageLoader"
 import { CacaoContext } from "lib/games/cacao/context"
 import { GameType } from "lib/games/types"
 
 import { Game } from "./Game"
 import { CacaoStoreProvider } from "./store"
+import { ForestImageSources } from "./Tile/ForestTile"
+
+const ImageSources = Object.values(ForestImageSources)
 
 export default function Cacao() {
   return (
-    <CacaoStoreProvider>
-      <GameProvider context={CacaoContext} game={GameType.CACAO}>
-        <Game />
-      </GameProvider>
-    </CacaoStoreProvider>
+    <ImageLoader images={ImageSources}>
+      <CacaoStoreProvider>
+        <GameProvider context={CacaoContext} game={GameType.CACAO}>
+          <Game />
+        </GameProvider>
+      </CacaoStoreProvider>
+    </ImageLoader>
   )
 }
