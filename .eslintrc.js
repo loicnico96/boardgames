4d2d2777a42344dad8c58e51588ba6720e7c6d50
loicnico96 <loicnico96@gmail.com>
2022-06-29 16:47:02 +0200

    Fix GitHub workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
+    project: ["tsconfig.json", "packages/*/tsconfig.json"],
     sourceType: "module",
   },
   plugins: ["@typescript-eslint", "import", "prettier"],
